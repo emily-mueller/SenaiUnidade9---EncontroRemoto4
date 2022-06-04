9119def0c5ccfe23944dbb43d2499b22c0092245
emily-mueller <emily_mueller@id.uff.br>
2022-06-03 21:10:43 -0300

    Criacao do Objeto PessoaJuridica

---
+++ Classes/Pessoa.cs
@@ -0,0 +1,16 @@
+using EncontroRemoto4.Interfaces;
+
+namespace EncontroRemoto4.Classes
+{
+    public abstract class Pessoa : IPessoa
+    {
+        public string? Nome { get; set; }
+        
+        public float Rendimento { get; set; }
+
+        public Endereco? Endereco { get; set; }
+
+        public abstract float PagarImposto(float rendimento);
+        
+    }
+}
---
+++ Interfaces/IPessoa.cs
@@ -0,0 +1,7 @@
+namespace EncontroRemoto4.Interfaces
+{
+    public interface IPessoa
+    {
+         float PagarImposto(float rendimento);
+    }
+}
